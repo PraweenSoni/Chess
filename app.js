97032b99d89f4f7f2dba1fa0904bdee21ce0e631
PraweenSoni <pramrajkumar1234@gmail.com>
2024-05-28 19:30:00 +0530

    V1.0.0

---
+++ app.js
@@ -0,0 +1,75 @@
+const express = require("express");
+const socket = require("socket.io");
+const http = require("http");
+const { Chess }= require("chess.js");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const io = socket(server);
+
+const chess = new Chess();
+
+let players = {};
+let currentPlayer = "w";
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req,res)=>{
+    res.render("index", {title : "Online Chess game"});
+});
+
+io.on("connection",function(uniquesocket){
+    console.log("Player Connected");
+    // uniquesocket.on("bol",function(){
+    //     io.emit("Haa");
+    // })
+    if(!players.white){
+        players.white = uniquesocket.id;
+        uniquesocket.emit("playerRole","w");
+    }
+    else if(!players.black){
+        players.black = uniquesocket.id;
+        uniquesocket.emit("playerRole","b");
+    }else{
+        uniquesocket.emit("spectatorRole");
+    }
+
+    // Player offline code..
+    uniquesocket.on("disconnect", function(){
+        if(uniquesocket.id === players.white){
+            delete players.white;
+            console.log("White Go Offline");
+        }else if(uniquesocket.id === players.black){
+            delete players.black;
+            console.log("Black Go Offline");
+        }
+    });
+
+    uniquesocket.on("move", (move)=>{
+        try{
+            if(chess.turn() == 'w' && uniquesocket.id !== players.white) return;
+            if(chess.turn() == 'b' && uniquesocket.id !== players.black) return;
+
+            const result = chess.move(move);
+            if(result){
+                currentPlayer = chess.turn();
+                io.emit("move", move);
+                io.emit("boardState", chess.fen());
+            }else{
+                console.log("Invalid Move :", move);
+                uniquesocket.emit("Invalid Move ", move);
+            }
+        } 
+        catch(err){
+            console.log(err);
+            uniquesocket.emit("Invalid Move :", move);
+        }
+    });
+});
+
+server.listen(3000,function(){
+    console.log("Server Started! 3000");
+});
+
