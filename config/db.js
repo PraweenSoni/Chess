c032708980fef43674af99d8c6d95a47677584fe
Praween Soni <pramrajkumar1234@gmail.com>
2025-06-24 17:07:41 +0530

    DB connection issue for render

--- config/db.js
+++ config/db.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
 // mongoose.set('debug', true);
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => console.log('----------< Connected to MongoDB >-----------'))
-  .catch(err => console.error(err));
+mongoose.connect(process.env.MONGODB_URI, {
+  tls: true,             // force TLS, avoids Render TLSv1.3 bug
+  family: 4,             // prefer IPv4 (Render DNS issues are rare but possible)
+  socketTimeoutMS: 10000 // optional: timeout if DB is slow to respond
+})
+  .then(() => console.log('----------< Connected to MongoDB Atlas >-----------'))
+  .catch(err => {
+    console.error('MongoDB connection error:', err.message);
+    // Optional: prevent app crash
+    // process.exit(1); 
+  });
 
 module.exports = mongoose;
