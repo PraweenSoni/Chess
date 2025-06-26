8824dcb1f35c58413437459988c012ec3bf7c8b7
Praween Soni <pramrajkumar1234@gmail.com>
2025-06-26 23:10:27 +0530

    some changes on dynamic profile/username route

--- routes/user.js
+++ routes/user.js
@@ -1,20 +1,26 @@
 const express = require("express");
 const User = require("../models/User");
+const UserInfo = require("../models/UserInfo");
 
 const router = express.Router();
 
-router.get('/profile/:username',async (req, res) => {
+router.get('/profile/:username', async (req, res) => {
   const username = req.params.username;
 
-  const user = await User.findOne({username : username});
-  // if (!user) {
-  //   return res.redirect('/login');
-  // }
-  
-  if(!user){
+  const user = await User.findOne({ username: username });
+  const userInfo = await UserInfo.findOne({ userId: user._id });
+
+  if (!user) {
     res.status(400).send('usernmae is required!! Or user will not avaliable');
-  }else{
-    res.render('user/profile', { user: { name: user.username, createdAt: user.createdAt} });
+  } else {
+    res.render('user/userprofile', {
+      user: {
+        username: user.username,
+        createdAt: user.createdAt,
+        isLoggedIn: false,
+      },
+      userInfo
+    });
   }
 });
 
