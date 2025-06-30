8cf67ff29d2af72821053b728ce5a14d81d16272
Praween Soni <pramrajkumar1234@gmail.com>
2025-06-30 19:54:12 +0530

    Organize Setp 1

---
+++ backup file/db.js
@@ -0,0 +1,32 @@
+// // for local env
+
+// const mongoose = require('mongoose');
+
+// mongoose.connect(process.env.MONGODB_URI)
+//   .then(() => console.log('*----------< Connected to MongoDB >-----------*'))
+//   .catch(err => console.error(err));
+
+// module.exports = mongoose;
+
+// // for production
+
+// const mongoose = require('mongoose');
+
+// async function connectDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       tls: true,              // force TLS encryption
+//       family: 4,              // prefer IPv4
+//       socketTimeoutMS: 10000, // quick fail if no connection
+//     });
+//     console.log('✅ Connected to MongoDB Atlas');
+//   } catch (err) {
+//     console.error('❌ MongoDB connection failed:', err.message);
+//     console.error('💥 Full error:', err);
+//     process.exit(1);
+//   }
+// }
+
+// connectDB();
+
+// module.exports = mongoose;
