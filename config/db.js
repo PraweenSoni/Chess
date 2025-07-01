7f1c6f795eb65b0d2ef54a185f977941e7495c74
Praween Soni <pramrajkumar1234@gmail.com>
2025-07-01 22:18:44 +0530

    small changes

--- config/db.js
+++ config/db.js
@@ -1,20 +1,23 @@
 const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('*----------< Connected to MongoDB >-----------*'))
+  .catch(err => console.error(err));
 
-async function connectDB() {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI, {
-      tls: true,              // force TLS encryption
-      family: 4,              // prefer IPv4
-      socketTimeoutMS: 10000, // quick fail if no connection
-    });
-    console.log('✅ Connected to MongoDB Atlas');
-  } catch (err) {
-    console.error('❌ MongoDB connection failed:', err.message);
-    console.error('💥 Full error:', err);
-    process.exit(1);
-  }
-}
+// async function connectDB() {
+//   try {
+//     await mongoose.connect(process.env.MONGODB_URI, {
+//       tls: true,              // force TLS encryption
+//       family: 4,              // prefer IPv4
+//       socketTimeoutMS: 10000, // quick fail if no connection
+//     });
+//     console.log('✅ Connected to MongoDB Atlas');
+//   } catch (err) {
+//     console.error('❌ MongoDB connection failed:', err.message);
+//     console.error('💥 Full error:', err);
+//     process.exit(1);
+//   }
+// }
 
-connectDB();
+// connectDB();
 
 module.exports = mongoose;
